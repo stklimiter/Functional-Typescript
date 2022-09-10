021292829a3857a844244763319c03e7cb54c949
stklimiter <Tim.Kristel@gmail.com>
2022-09-10 13:52:37 +0200

    Added better yielder

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,31 +1,48 @@
-import {Functor} from "./Functor";
-import {Monad} from "./Monad";
 import {compose} from "./Compose";
+import {Maybe} from "./Maybe";
 
-type RootNode = null
-type Node<T> = [value: T, next: Node<T>] | RootNode
-
+type NoneEmptyNode<T> = [value: T, next: Node<T>]
+type Node<T> = NoneEmptyNode<T> | None
 type None = null
+
 type Some<T> = T
-type Maybe<T> = Some<T> | None
 type reduceFn<T> = (toCollect: T, collector: Node<T>) => Node<T>
 type CreateNodeFn<T> = (gen: Generator<T>) => Node<T>
 type CreatNodeType = <T>(gen: Generator<T>, previous: Node<T>) => Node<T>
 
-const unwrap: <T>(n: Node<T>) => Maybe<T> | null = n => n ? n[0] : null
+const unwrap: <T>(n: Node<T>) => Maybe<T> = n => Maybe.of(n).map(i => i[0])
 const next: <T>(n: Node<T>) => Node<T> = n => n ? n[1] : null
-const emptyNode: RootNode = null
+const emptyNode: None = null
 const identity: <T>(n: T) => Node<T> = n => [n, null]
 const push: <T>(n: Node<T>, c: T) => Node<T> = (n, v) => [v, n]
 const pop: <T>(n: Node<T>) => [Maybe<T>, Node<T>] = (n) => [unwrap(n), next(n)]
 
-const createNode = <T>(gen: Generator<T>, previous: Node<T> = null): Node<T> => {
+const isEmpty: <T>(n: Node<T>) => n is None = (n): n is None => n === emptyNode
+const map =  <T, L>(n: Node<T>, mapFn: (value: T) => L): Node<L> => isEmpty(n) ? n as Node<L> : [mapFn(n[0]), map(n[1], mapFn)]
+
+const reduce = <T, I>(callbackfn: (previousValue: I, currentValue: T) => I, node: Node<T>, initialValue: I): I => !isEmpty(node) ? reduce(callbackfn, node[1], callbackfn(initialValue, node[0])) : initialValue
+
+const createNodeReverse = <T>(gen: Generator<T>, previous: Node<T> = null): Node<T> => {
     const {value, done} = gen.next()
-    return done ? previous : createNode(gen, [value, previous])
+    return done ? previous : createNodeReverse(gen, [value, previous])
 }
-const createNodeReverse = <T>(gen: Generator<T>): Node<T> => {
+const createNode = <T>(gen: Generator<T>): Node<T> => {
     const {value, done} = gen.next()
-    return [value, done ? null : createNodeReverse(gen)]
+    return done ? null : [value, createNode(gen)]
+}
+
+const yieldAll = function* <T>(n: Node<T>): Generator<T> {
+    if (!isEmpty(n)) {
+        yield n[0]
+        yield* yieldAll(n[1])
+    }
+}
+
+const yieldAllReverse = function* <T>(n: Node<T>): Generator<T> {
+    if (!isEmpty(n)) {
+        yield* yieldAll(n[1])
+        yield n[0]
+    }
 }
 
 const toGen = <T>(arr: Iterable<T>): Generator<T> => function* <T>() {
@@ -35,8 +52,8 @@ const toGen = <T>(arr: Iterable<T>): Generator<T> => function* <T>() {
 /**
  * n pointing to n-1, n-1 pointing to n-2
  */
+const createReverse: <T>(itt: Iterable<T>) => Node<T> = compose(toGen, createNodeReverse)
 const create: <T>(itt: Iterable<T>) => Node<T> = compose(toGen, createNode)
-const createReverse: <T>(itt: Iterable<T>) => Node<T> = compose(Node.toGen, Node.createNodeReverse)
 
 
 /**
@@ -45,20 +62,29 @@ const createReverse: <T>(itt: Iterable<T>) => Node<T> = compose(Node.toGen, Node
 
 export class LinkedList<T> implements Iterable<T> {
     constructor(private root: Node<T>) {
+
     }
 
     private static of = <T>(root: Node<T>) => new LinkedList(root)
 
-    static create = compose(Node.create, LinkedList.of)
-    static createReverse = compose(Node.createReverse, LinkedList.of)
+    static create = compose(create, LinkedList.of)
+    static createReverse = compose(createReverse, LinkedList.of)
 
     public pop(): [Maybe<T>, LinkedList<T>] {
-        const [maybe, node] = Node.pop(this.root)
+        const [maybe, node] = pop(this.root)
         return [maybe, LinkedList.of(node)]
     }
 
+    public getSize() {
+        return reduce((value, _) => value + 1, this.root, 0)
+    }
+
+    public isEmpty(): boolean {
+        return this.root == emptyNode
+    }
+
     public push(value: T): LinkedList<T> {
-        return LinkedList.of(Node.push(this.root, value))
+        return LinkedList.of(push(this.root, value))
     }
 
     public reverse(): LinkedList<T> {
@@ -66,11 +92,7 @@ export class LinkedList<T> implements Iterable<T> {
     }
 
     * [Symbol.iterator](): Iterator<T> {
-        let pointer = this.root
-        while (pointer != null) {
-            yield pointer[0]
-            pointer = pointer[1]
-        }
+        yield* yieldAll(this.root)
     }
 }
 
--- src/Maybe.ts
+++ src/Maybe.ts
@@ -2,32 +2,53 @@ export interface Maybe<K> {
 
     map<T>(mapFunction: (from: K) => T): Maybe<T>
 
+    isEmpty(): this is None<K>
+
+    orElse(supplier: () => K): K
 }
 
-class EmptyMaybe<K> implements Maybe<K> {
+class None<K> implements Maybe<K> {
     map<T>(mapFunction: (from: K) => T): Maybe<T> {
         return empty as Maybe<T>;
     }
+
+    isEmpty(): this is None<K> {
+        return true;
+    }
+
+    orElse(supplier: () => K): K {
+        return supplier()
+    }
 }
 
-class NonEmptyMaybe<K> implements Maybe<K> {
+class Some<K> implements Maybe<K> {
     constructor(private value: K) {
     }
 
     map<T>(mapFunction: (from: K) => T): Maybe<T> {
-        return new NonEmptyMaybe(mapFunction(this.value));
+        return new Some(mapFunction(this.value));
+    }
+
+    isEmpty(): this is None<K> {
+        return false;
+    }
+
+    orElse(supplier: () => K): K {
+        return this.value;
     }
 }
 
-const empty = new EmptyMaybe()
+const empty = new None()
 
 function of<K>(value: K | null | undefined): Maybe<K> {
     if (value)
-        return new NonEmptyMaybe<K>(value)
+        return new Some<K>(value)
     return empty as Maybe<K>
 }
 
 export const Maybe = {
-    empty: new EmptyMaybe(),
+    empty<T>(): Maybe<T> {
+        return empty as Maybe<T>
+    },
     of: of
 }
